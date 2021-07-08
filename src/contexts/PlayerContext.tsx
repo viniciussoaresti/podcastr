0e6f29d12171149cc71f0af6ff9d16f5e8543378
Vinícius Soares <viniciussoaresti@gmail.com>
2021-07-08 07:01:09 -0300

    feat(#1): class 5 1/4

---
+++ src/contexts/PlayerContext.tsx
@@ -0,0 +1,50 @@
+import { createContext, useState, ReactNode } from 'react';
+
+interface Episode {
+    title: string;
+    members: string;
+    thumbnail: string;
+    duration: number;
+    url: string;
+};
+
+interface PlayerContextData {
+    episodeList: Array<Episode>;
+    currentEpisodeIndex: number;
+    isPlaying: boolean;
+    play: (episode: Episode) => void;
+    setPlayingState: (state: boolean) => void;
+    togglePlay: () => void;
+};
+
+export const PlayerContext = createContext({} as PlayerContextData);
+
+interface PlayerContextProviderProps {
+    children: ReactNode;
+};
+
+export function PlayerContextProvider({ children }: PlayerContextProviderProps) {
+    const [episodeList, setEpisodeList] = useState([]);
+    const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
+    const [isPlaying, setIsPlaying] = useState(false);
+
+    function play(episode: Episode) {
+        setEpisodeList([episode]);
+        setCurrentEpisodeIndex(0);
+        setIsPlaying(true);
+    }
+
+    function togglePlay() {
+        setIsPlaying(!isPlaying);
+    }
+
+    function setPlayingState(state: boolean) {
+        setIsPlaying(state);
+    }
+
+    return (
+        <PlayerContext.Provider value={{ episodeList, currentEpisodeIndex, play, isPlaying, togglePlay, setPlayingState }}>
+            {children}
+        </PlayerContext.Provider>
+    )
+}
